45bd08c2c47d52cd9a5acff23a8bf9d279b5a3b2
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-03 14:36:16 -0400

    deleted duplicate data from event gamers

--- db.sql
+++ db.sql
@@ -1,7 +1,8 @@
 SELECT * FROM levelupapi_gamer;
 
+SELECT * FROM levelupapi_eventgamer;
 
-SELECT * FROM levelupapi_event;
+DELETE FROM levelupapi_eventgamer WHERE gamer_id = 2;
 
 INSERT INTO levelupapi_gamer (id, uid, bio)
 VALUES (1, "123456asddfg123456asddfg", "My name is Hex");
